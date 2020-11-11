bbf9afb213acbede29a698fee92843464a5e6de5
FRANCESCO TONELLI <francesco.tonelli.stud@ispascalcomandini.it>
2020-11-11 10:30:10 +0000

    Secondo esempio: uccisione di un thread

--- main.cs
+++ main.cs
@@ -7,6 +7,7 @@ namespace IntroduzioneThread
     {
         static void Main(string[] args)
         {
+            Console.Clear();
                                        // Delegato      // Metodo
             Thread t1 = new Thread(new ThreadStart(Threads.Thread1));   //Oggetto per il primo thread, si riferisce al metodo "Threads.Thread1"
             Thread t2 = new Thread(new ThreadStart(Threads.Thread2
@@ -16,13 +17,22 @@ namespace IntroduzioneThread
             t1.Start(); //Avvia primo thread
             t2.Start(); //Avvia secondo thread
 
+
+            //l'uccisione di un processo spesso genera un'eccezione, quindi
+            try{
+              t1.Abort();
+            }
+            catch{
+              Console.WriteLine("Eccezione per l'uccisione di un thread");
+            }
+
+
             //Ciclo che verrà eseguito in parallelo
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 100; i++)
             {
                 Console.WriteLine("Main: " + i);
             }
 
-            Console.ReadKey();
         }
     }
 
@@ -30,7 +40,7 @@ namespace IntroduzioneThread
     {
         public static void Thread1() //Metodo del primo thread
         {
-            for(int i = 0; i < 10; i++)
+            for(int i = 0; i < 100; i++)
             {
                 Console.WriteLine("Thread 1: " + i);
             }
@@ -38,7 +48,7 @@ namespace IntroduzioneThread
 
         public static void Thread2() //Metodo del secondo thread
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 100; i++)
             {
                 Console.WriteLine("Thread 2: " + i);
             }
